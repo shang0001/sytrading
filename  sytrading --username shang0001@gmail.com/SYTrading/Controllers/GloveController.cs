b279002bcd7b46081b074c4942ca86d2083e1ff0
shang0001@gmail.com <shang0001@gmail.com@1acb569a-5b99-cab8-9d33-0e84391dbbaf>
2013-07-11 04:08:02 +0000

    1. Change textbox to dropdownlist for the "Level" field in Config/Edit.cshtml for consistent. 2. Add "Release" field in Glove/Details.cshtml and Glove/Edit.cshtml. This also fix the issue that editing a glove will make it "Not Released". 3. Support for cache clear (Glove module).

---  sytrading --username shang0001@gmail.com/SYTrading/Controllers/GloveController.cs
+++  sytrading --username shang0001@gmail.com/SYTrading/Controllers/GloveController.cs
@@ -6,6 +6,8 @@ using System.Data.Entity.Infrastructure;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using SYTrading.Models;
@@ -112,6 +114,10 @@ namespace SYTrading.Controllers
                 }
 
                 db.SaveChanges();
+
+                // Very costly! 
+                // But have to clear cache for all gloves because the related gloves list in Glove's detail page might include this new entry.
+                TempData.Add("ClearCache", ClearCache("Glove", null, null));
 
                 return RedirectToAction("Index");
             }
@@ -239,8 +245,11 @@ namespace SYTrading.Controllers
                 db.Entry(gloveViewData.Glove).State = EntityState.Modified;
                 db.SaveChanges();
 
+                TempData.Add("ClearCache", ClearCache("Glove", gloveViewData.Glove.GloveID, null));
+
                 return RedirectToAction("Index");
             }
+
             ViewBag.MaterialID = new SelectList(db.Materials, "MaterialID", "Name", gloveViewData.Glove.MaterialID);
             return View(gloveViewData.Glove);
         }
@@ -285,6 +294,9 @@ namespace SYTrading.Controllers
             }
             db.Gloves.Remove(glove);
             db.SaveChanges();
+
+            TempData.Add("ClearCache", ClearCache("Glove", null, null));
+
             return RedirectToAction("Index");
         }
 
@@ -332,6 +344,9 @@ namespace SYTrading.Controllers
                 }
             }
             db.SaveChanges();
+
+            TempData.Add("ClearCache", ClearCache("Glove", null, null));
+
             return RedirectToAction("Index");
         }
 
@@ -347,6 +362,12 @@ namespace SYTrading.Controllers
                 }
             }
 
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ClearCache()
+        {
+            TempData.Add("ClearCache", ClearCache("Glove", null, null));
             return RedirectToAction("Index");
         }
 
@@ -373,6 +394,74 @@ namespace SYTrading.Controllers
 
                 return Path.Combine(absolutePath);
             }
+        }
+
+        private string ClearCache(string target, int? id, bool? clearIndexOnly)
+        {
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the 
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            string baseUrl = string.Empty;
+            if (System.Web.HttpContext.Current.IsDebuggingEnabled && db.Configs.Where(c => c.Name == "BaseWebUrl" && c.Level == "Debug").Count() > 0)
+            {
+                baseUrl = db.Configs.Where(c => c.Name == "BaseWebUrl" && c.Level == "Debug").Single().Value;
+            }
+            else if ((!System.Web.HttpContext.Current.IsDebuggingEnabled) && (db.Configs.Where(c => c.Name == "BaseWebUrl" && c.Level == "Release").Count() > 0))
+            {
+                baseUrl = db.Configs.Where(c => c.Name == "BaseWebUrl" && c.Level == "Release").Single().Value;
+            }
+
+            StringBuilder result = new StringBuilder();
+            if (!string.IsNullOrEmpty(baseUrl))
+            {
+                List<string> urls = new List<string>();
+                switch (target)
+                {
+                    case "Glove":
+                        if (id.HasValue)
+                        {
+                            // Clear for index and one glove detail page
+                            urls.Add(baseUrl + "/Glove/clearCache/" + id);
+                        }
+                        else if (clearIndexOnly.HasValue && clearIndexOnly.Value == true)
+                        {
+                            // Clear for index ONLY
+                            urls.Add(baseUrl + "/Glove/clearCache?clearIndexOnly=true");
+                        }
+                        else
+                        {
+                            // Clear for index and all gloves detail pages
+                            urls.Add(baseUrl + "/Glove/clearCache");
+                        }
+                        break;
+                    case "Home":
+                        urls.Add(baseUrl + "/Home/clearCache");
+                        break;
+                    default:
+                        urls.Add(baseUrl + "/Glove/clearCache");
+                        urls.Add(baseUrl + "/Home/clearCache");
+                        break;
+                }
+
+                foreach (string url in urls)
+                {
+                    Stream data = client.OpenRead(url);
+                    StreamReader reader = new StreamReader(data);
+                    result.AppendLine(string.Format("{0} : {1}", url, reader.ReadToEnd()));
+                    data.Close();
+                    reader.Close();
+                }
+            }
+            else
+            {
+                result.AppendLine("No Base URL");
+            }
+
+            return result.ToString();
         }
     }
 }
