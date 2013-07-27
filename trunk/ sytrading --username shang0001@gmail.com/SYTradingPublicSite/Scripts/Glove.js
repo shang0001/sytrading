0820c1a71c4cfec4749c7adda4f016ac7d1cd4bd
shang0001@gmail.com <shang0001@gmail.com@1acb569a-5b99-cab8-9d33-0e84391dbbaf>
2013-07-27 02:34:25 +0000

    1. Add "Top" icon at the bottom of page for phone view. 2. Modify the position of the "Left" icon for phone view. 3. For phone view, change the listview style in the Glove's index and detail page. Remove gaps, radius corner, and make it fully wide. 4. Change image slides in the Home/Index.cshtml. Now it is carousel for non-phone view and slide for phone view. 5. Fix issues for IE7 compatibility 6. Add "empty message" in Glove/Index.cshtml to tell user no glove.

--- trunk/ sytrading --username shang0001@gmail.com/SYTradingPublicSite/Scripts/Glove.js
+++ trunk/ sytrading --username shang0001@gmail.com/SYTradingPublicSite/Scripts/Glove.js
@@ -1,15 +1,25 @@
-﻿// Enable flexslider2 for Home page
-$('.flexslider').flexslider({
-    animation: "slide"
-});
-
-$("#pageslide_openbar").pageslide({ href: '#', direction: "left", iframe: false, overlay: true });
+﻿$("#pageslide_openbar").pageslide({ href: '#', direction: "left", iframe: false, overlay: true });
 
 if ($('p[class="hidden-phone"]').css("display") != "none") {
     // For detail page, enable colorbox
     $(".group1").colorbox({ rel: 'group1', height: "100%" });
+
+    // Enable flexslider2 for Home page
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        itemWidth: 450,
+        itemMargin: 5,
+        minItems: 2,
+        maxItems: 4
+    });
 }
 else {
+    // Enable flexslider2 for Home page
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+
     // For glove list, make listview item take tap/click event
     $('.show-grid [class*="span"]').click(function () {
         location.href = $("a", this).attr("href");
